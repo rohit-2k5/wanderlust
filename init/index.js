f7fd6c9aa705315d9627ff75d5c7973fcf9e4241
rohit kumar <rohitkumaraf99@gmail.com>
2025-06-24 13:43:05 +0530

    fix: use same Atlas DB url for init

--- init/index.js
+++ init/index.js
@@ -2,7 +2,8 @@
 const mongoose = require("mongoose");
 const initdata = require("./data.js");
 const Listing = require("../models/listing.js");
-const dbUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/wanderlust";
+
 
 main()
 .then(()=>{
